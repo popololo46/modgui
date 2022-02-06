c8f5947587f5120d6ecf115edd07ae8a7922a839
popololo46 <75115409+popololo46@users.noreply.github.com>
2022-02-06 14:13:13 +0100

    script to install auto

---
+++ install_modgui.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+sh /3BandEQ.lv2/install_3BandEQ_modgui.sh
+
+sh /3BandSplitter.lv2/install_3BandSplitter_modgui.sh
+
+sh /alo.lv2/install_alo_modgui.sh
+
+sh /amsynth.lv2/install_amsynth_modgui.sh
+
+sh /argotlunar2.lv2/install_argotlunar_modgui.sh
+
+sh /dexed.lv2/install_dexed_modgui.sh
+
+ exit
